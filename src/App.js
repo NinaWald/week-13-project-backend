17ab699ea1dd1ab02e3f228870cdea68b3f69fa0
NinaW <120198299+NinaWald@users.noreply.github.com>
2023-05-06 16:05:19 +0200

    fixing

--- src/App.js
+++ src/App.js
@@ -1,40 +1,53 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
+
+const getMovieByTitle = async (title) => {
+  const response = await fetch(`https://project-express-api-y6ibchp5wa-lz.a.run.app/movies/${title}`);
+  const data = await response.json();
+  if (data.status === 'success') {
+    return data;
+  } else {
+    throw new Error(data.message);
+  }
+};
 
 export const App = () => {
-  const [moviesList, setMoviesList] = useState([]);
   const [searchTerm, setSearchTerm] = useState('');
+  const [movie, setMovie] = useState(null);
+  const [error, setError] = useState('');
 
-  useEffect(() => {
-    fetch('https://project-express-api-y6ibchp5wa-lz.a.run.app/movies')
-      .then((response) => response.json())
-      .then((data) => {
-        setMoviesList(data)
-      })
-  }, [])
-
-  const handleSearch = (event) => {
-    setSearchTerm(event.target.value);
+  const handleSearch = async (title) => {
+    try {
+      const data = await getMovieByTitle(title);
+      setMovie(data.body.movie);
+      setError('');
+    } catch (err) {
+      setMovie(null);
+      setError('No movie found');
+    }
   };
 
-  const filteredMovies = moviesList.filter((movie) =>
-    // eslint-disable-next-line implicit-arrow-linebreak
-    movie.title.toLowerCase().includes(searchTerm.toLowerCase()));
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    handleSearch(searchTerm);
+  };
 
   return (
-    <div>
-      <form>
-        <input
-          type="text"
-          placeholder="Search for a movie..."
-          value={searchTerm}
-          onChange={handleSearch} />
+    <div className="App">
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="form">
+          Search for a movie:
+          <input type="text" value={searchTerm} onChange={(event) => setSearchTerm(event.target.value)} />
+        </label>
+        <button type="submit">Search</button>
       </form>
-      {filteredMovies.map((movie) => (
-        <div key={movie.show_id}>
+      {error && <p>{error}</p>}
+      {movie && (
+        <div>
           <h2>{movie.title}</h2>
+          <h2>{movie.release_year}</h2>
           <p>{movie.description}</p>
         </div>
-      ))}
+      )}
     </div>
   );
 };
