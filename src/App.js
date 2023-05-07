4d10ceb66ffee785c2080a1c661a3038ca3b40be
NinaW <120198299+NinaWald@users.noreply.github.com>
2023-05-07 18:22:04 +0200

    css

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import AllMovies from './components/AllMovies';
+import './index.css';
 import { SearchMovies } from './components/SearchMovies';
 
 export const App = () => {
   return (
     <div className="firstpage">
       <BrowserRouter>
-        <h1>hello and welcome to movie api</h1>
+        <h1>Hello and welcome to Ninas first express api!</h1>
         <nav>
           <ul>
             <li>
